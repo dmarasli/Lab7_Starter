ccb26a7ab921aa3f9f3416197fcf54adb8d9d316
dmarasli <dmarasli@ucsd.edu>
2021-11-05 13:17:49 -0700

    done by step 5

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,9 @@ const router = new Router(function () {
    * This will only be two single lines
    * If you did this right, you should see just 1 recipe card rendered to the screen
    */
+  document.querySelector('.section--recipe-cards').classList.add('shown');
+  document.querySelector('.section--recipe-expand').classList.remove('shown');
+
 });
 
 window.addEventListener('DOMContentLoaded', init);
@@ -55,6 +58,16 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
+
 }
 
 /**
@@ -89,25 +102,31 @@ async function fetchRecipes() {
  */
 function createRecipeCards() {
   // Makes a new recipe card
+  let i = 0;
+  for (let property in recipeData){
+  
   const recipeCard = document.createElement('recipe-card');
+  i++;
   // Inputs the data for the card. This is just the first recipe in the recipes array,
   // being used as the key for the recipeData object
-  recipeCard.data = recipeData[recipes[0]];
+  recipeCard.data = recipeData[property];
 
   // This gets the page name of each of the arrays - which is basically
   // just the filename minus the .json. Since this is the first element
   // in our recipes array, the ghostCookies URL, we will receive the .json
   // for that ghostCookies URL since it's a key in the recipeData object, and
   // then we'll grab the 'page-name' from it - in this case it will be 'ghostCookies'
-  const page = recipeData[recipes[0]]['page-name'];
+  const page = recipeData[property]['page-name'];
   router.addPage(page, function() {
     document.querySelector('.section--recipe-cards').classList.remove('shown');
     document.querySelector('.section--recipe-expand').classList.add('shown');
-    document.querySelector('recipe-expand').data = recipeData[recipes[0]];
+    document.querySelector('recipe-expand').data = recipeData[property];
   });
   bindRecipeCard(recipeCard, page);
-
+  
   document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
+  if (i > 3) recipeCard.classList.add('hidden');
+  }
 
   /**
    * TODO - Part 1 - Step 3
@@ -119,6 +138,10 @@ function createRecipeCards() {
    * After this step you should see multiple cards rendered like the end of the last
    * lab
    */
+
+  
+ 
+
 }
 
 /**
@@ -174,6 +197,16 @@ function bindEscKey() {
    * if the escape key is pressed, use your router to navigate() to the 'home'
    * page. This will let us go back to the home page from the detailed page.
    */
+  document.addEventListener('keydown', function(event){
+    if(event.key == "Escape"){
+      console.log("Escape button used");
+      router.navigate('home');
+    }
+  });
+
+
+
+  
 }
 
 /**
